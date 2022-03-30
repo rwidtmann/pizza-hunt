1c9464d8a8c2b135b9221ed2d43272a20f736f4b
Richard Widtmann <rwidtmann@sbcglobal.net>
2022-03-30 15:13:09 -0500

    End of 18.5.4

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,39 +5,38 @@ const pizzaController = {
   getAllPizza(req, res) {
     Pizza.find({})
       .populate({
-        path: 'comments',
-        select: '-__v'
+        path: "comments",
+        select: "-__v",
       })
-      .select('-__v')
+      .select("-__v")
       .sort({ _id: -1 })
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
         console.log(err);
         res.status(400).json(err);
       });
   },
 
   // get one pizza by id
-getPizzaById({ params }, res) {
-  Pizza.findOne({ _id: params.id })
-    .populate({
-      path: 'comments',
-      select: '-__v'
-    })
-    .select('-__v')
-    .then(dbPizzaData => {
-      if (!dbPizzaData) {
-        res.status(404).json({ message: 'No pizza found with this id!' });
-        return;
-      }
-      res.json(dbPizzaData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-},
-
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
   // createPizza
   createPizza({ body }, res) {
@@ -48,7 +47,10 @@ getPizzaById({ params }, res) {
 
   // update pizza by id
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
